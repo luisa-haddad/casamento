4615145f15a79575f15cede000ed18d7e0017659
luisa-haddad <luisa.haddad@gmail.com>
2019-09-30 10:22:57 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends Component {
   // our first get method that uses our backend api to
   // fetch data from our data base
   getDataFromDb = () => {
-    fetch('http://localhost:3001/api/getData')
+    fetch('http://casamentoluele-com.umbler.net:3001/api/getData')
       .then((data) => data.json())
       .then((res) => this.setState({ data: res.data }));
   };
@@ -59,7 +59,7 @@ class App extends Component {
       ++idToBeAdded;
     }
 
-    axios.post('http://localhost:3001/api/putData', {
+    axios.post('http://casamentoluele-com.umbler.net:3001/api/putData', {
       id: idToBeAdded,
       message: message,
     });
@@ -76,7 +76,7 @@ class App extends Component {
       }
     });
 
-    axios.delete('http://localhost:3001/api/deleteData', {
+    axios.delete('http://casamentoluele-com.umbler.net:3001/api/deleteData', {
       data: {
         id: objIdToDelete,
       },
@@ -94,7 +94,7 @@ class App extends Component {
       }
     });
 
-    axios.post('http://localhost:3001/api/updateData', {
+    axios.post('http://casamentoluele-com.umbler.net:3001/api/updateData', {
       id: objIdToUpdate,
       update: { message: updateToApply },
     });
