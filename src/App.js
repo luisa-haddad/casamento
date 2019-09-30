fece189c6f05684c76cb2f395b4053e011498203
luisa-haddad <luisa.haddad@gmail.com>
2019-09-30 13:35:22 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -140,7 +140,7 @@ class App extends Component {
             <div className="text">
               <h6>Onde devo ir ???</h6>
               
-              <p>Para Juquitiba, fica uns 76 Km de SP.<br/><a href="geo:-23.875611,-47.090389">Estrada Martha Maria de Jesus, 9500</a></p>
+              <p>Para Juquitiba, fica uns 76 Km de SP.<br/><a href="https://goo.gl/maps/Vt7f3Q8HPyzjRAjt6">Estrada Martha Maria de Jesus, 9500</a></p>
             </div>
           </div>
           <div id="levar" className="container">
